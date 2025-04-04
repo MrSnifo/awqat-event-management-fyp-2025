09ef34b10afec6b6a9a2009cdcad32b423f7e93f
Snifo <snifo@mail.com>
2025-04-04 21:21:44 +0100

    entry point

--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,25 @@
 <?php
-include '../views/home.php';
+// Get requested path and clean it
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$basePath = '/' . implode('/', array_slice(explode('/', str_replace('\\', '/', __DIR__)), -3));
+$cleanPath = str_replace($basePath, '', $path);
+
+switch ($cleanPath) {
+    case '/':
+    case '/home':
+        include __DIR__ . '/../views/home.php';
+        break;
+
+    case '/login':
+        include __DIR__ . '/../views/login.php';
+        break;
+
+    case '/404':
+        include __DIR__ . '/../views/404.php';
+        break;
+
+    default:
+        http_response_code(404);
+        break;
+}
 ?>
