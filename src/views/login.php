8ffac2aa6785378e954b3b91910eb317ac43eac3
CHbaha <110380378+CHbaha@users.noreply.github.com>
2025-04-04 19:14:37 +0100

    loginPage

--- src/views/login.php
+++ src/views/login.php
@@ -1,2 +1,33 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Login - Ouqat</title>
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login to Ouqat</h2>
+
+        <div class="input-group">
+            <input type="text" id="username" placeholder=" " required />
+            <label for="username">Username or Email</label>
+        </div>
+
+        <div class="input-group">
+            <input type="password" id="password" placeholder=" " required />
+            <label for="password">Password</label>
+        </div>
+
+        <div class="remember-container">
+            <input type="checkbox" id="rememberMe" />
+            <label for="rememberMe">Remember Me</label>
+        </div>
+
+        <button>Log In</button>
+
+        <p>Don't have an account? <a href="#">Sign Up</a></p>
+    </div>
+</body>
+</html>
