0ef7dc6297911cc79b4ba8858ab1243950182904
RayenBt7 <151744296+RayenBt7@users.noreply.github.com>
2025-04-06 16:16:58 +0100

    Update registration.php

--- src/views/registration.php
+++ src/views/registration.php
@@ -1,2 +1,53 @@
 <?php
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Sign UP - Ouqat</title>
+    <link rel="stylesheet" href="SignUp.css">
+</head>
+
+<body >
+	<div class="SignUp-container">
+		
+		<h2>Sign Up</h2>
+	  
+		<div class="input-group">
+		  <input type="text" id="username" placeholder=" " required />
+		  <label for="username">Username</label>
+		</div>
+	  
+		<div class="input-group">
+		  <input type="text" id="email" placeholder=" " required />
+		  <label for="email">Email</label>
+		</div>
+	  
+		<div class="input-group">
+		  <input type="password" id="password" placeholder=" " required />
+		  <label for="password">Password</label>
+		</div>
+	  
+		<div class="input-group">
+		  <input type="password" id="confirm_password" placeholder=" " required />
+		  <label for="confirm_password">Confirm Password</label>
+
+		</div>
+		<button>Create Account </button>
+			<p class="sign-text">
+				Already have an account?
+				<a href="#" class="sign-link">Sign In</a>
+			  </p>
+
+			  <a href="index.html" class="back-home-btn">← Back Home</a>
+
+	  
+	  
+			
+		</form>
+	</div>
+    
+</body>
+</html>
