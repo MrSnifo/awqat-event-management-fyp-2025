632fc1ca4812d6fd7811dde18db950f381bdb462
Snifo <snifo@mail.com>
2025-04-11 20:51:25 +0100

    tweaks login page

--- src/views/login.php
+++ src/views/login.php
@@ -8,6 +8,17 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Login - Ouqat</title>
+
+    <script>
+    (function() {
+      const savedTheme = localStorage.getItem('user-theme');
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      document.documentElement.classList.toggle(
+        'dark-theme', 
+        savedTheme ? savedTheme === 'dark' : prefersDark
+      );
+    })();
+    </script>
     <link rel="stylesheet" href="assets/css/login.css">
 </head>
 
@@ -35,42 +46,6 @@
         <button>Log In</button>
 
         <p>Don't have an account? <a href="#"><label for="">Sign Up</label></a></p>
-
-        <div class="btn"><a href="C:\xampp\htdocs\PFA-2024-2025test\src\views\home.php"> Back Home</a></div>
     </form>
 </body>
 </html>
-
-<?php
-/*
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        
-        $Email = filter_input(INPUT_POST,"mail",FILTER_SANITIZE_SPECIAL_CHARS);
-        $password = filter_input(INPUT_POST,"password",FILTER_SANITIZE_SPECIAL_CHARS);
-
-        
-        if(empty($Email)){
-            echo "Please Enter an Email !!";
-        }
-        elseif(empty($password)){
-            echo "Please Enter a Password !!";
-        }else{
-            
-            $hash = password_hash($password,PASSWORD_DEFAULT);
-            $sql = "INSERT INTO users_login (user_mail , password) values ('$Email','$hash')";
-
-            try{
-                mysqli_query($conn, $sql);
-                echo "you are regestired ";
-                header("Location: Login.php");
-            }catch(mysqli_sql_exception){
-                echo "Email Exsists ";
-            }
-        }
-
-    }
-    mysqli_close($conn);
-
-
-*/
-?>
