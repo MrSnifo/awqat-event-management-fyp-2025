6265fb66b78feed061bf3f88e3b23d37e54eea2d
Snifo <snifo@mail.com>
2025-04-25 22:44:19 +0100

    python

--- src/config/python.php
+++ src/config/python.php
@@ -1,6 +1,7 @@
 <?php
 class Python {
     // 7l console ou ektb "where python" ou ma tnsech bch t3ml install lel requirements mwjodin fl scripts/requirements.txt 
+    // <!> You need to update this.
     public $binary = 'C:\Users\Snifo\AppData\Local\Programs\Python\Python313\python.exe';
 
     // Don't change this.
